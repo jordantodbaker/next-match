04cc74d01587bb3033cf987fab1f218766f597af
Jordan Baker <jordantodbaker@gmail.com>
2025-05-14 09:34:54 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/button";
+import Link from "next/link";
+import { FaRegSmile } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl">Hello app!</h1>
+      <Button color="primary" variant="bordered" as={Link} href="/members" startContent={<FaRegSmile size="20" />}>Click Me</Button>
+    </div>
+  );
+}
---
+++ src/components/Providers.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { HeroUIProvider } from '@heroui/react'
+import React, { ReactNode } from 'react'
+
+export default function Providers({ children }: { children: ReactNode }) {
+    return (
+        <HeroUIProvider>{children}</HeroUIProvider>
+    )
+}
