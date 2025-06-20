317f54a1bc1ec56b72e51eda922ea6486759603a
Jordan Baker <jordantodbaker@gmail.com>
2025-06-20 12:09:05 -0700

    Remove frame border

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -14,7 +14,6 @@ export default async function AdminPage() {
         width="1200"
         height="673.5"
         src="https://app.powerbi.com/view?r=eyJrIjoiZWU2NzE0YjgtMDg0Yi00YThjLTlmYWItYjNjMmU2ZTRkNTUzIiwidCI6ImYyODhkNTU2LTJmZTctNDkwZS1iYzdlLTFhMzcwYTRlNTU2ZiIsImMiOjZ9"
-        frameborder="0"
         allowFullScreen={true}
       ></iframe>
       {/* <iframe
